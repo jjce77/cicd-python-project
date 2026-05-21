a352085f4188a0236d12a9b4d78a06a552a4b9a8
JJCE <jjce77@gmail.com>
2026-05-21 12:41:45 -0500

    test: adding duplicate to test sonarqube

--- app/words.py
+++ app/words.py
@@ -1,4 +1,7 @@
 def build_word(words):
     """Task 3 placeholder."""
-    ## TODO: fix sonar warning
+    raise NotImplementedError
+
+def build_word_duplicate(words):
+    """Task 3 placeholder."""
     raise NotImplementedError
