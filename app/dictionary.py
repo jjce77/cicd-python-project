2b5e4565dec4f0220eb5efdc33528aaa9e01158d
JJCE <jjce77@gmail.com>
2026-05-21 14:07:51 -0500

    feat: add dictionary implementation

--- app/dictionary.py
+++ app/dictionary.py
@@ -1,3 +1,13 @@
 class Dictionary:
-    """Task 1 placeholder."""
+    def __init__(self) -> None:
+        self._entries: dict[str, str] = {}
+
+    def newentry(self, palabra: str, definicion: str) -> None:
+        self._entries[palabra] = definicion
+
+    def look(self, palabra: str) -> str:
+        if palabra in self._entries:
+            return self._entries[palabra]
+
+        return f"Can't find entry for {palabra}"
 
